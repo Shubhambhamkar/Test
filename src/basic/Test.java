b15237e31897da2260851e9ac337f74b604d0571
ASUS <ASUS@Shubhu>
2022-11-29 12:21:39 +0530

    First Commit

---
+++ src/basic/Test.java
@@ -0,0 +1,22 @@
+package basic;
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		String hungery="yess";
+		if (hungery=="yess")
+		{
+			System.out.println("khaay kahitari");
+			
+		} 
+		else
+		{
+			System.out.println("gapp bas");
+
+		}
+		
+
+	}
+
+}
