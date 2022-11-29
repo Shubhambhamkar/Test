03e5f39ed1310c83f315531d10ede87989f3a3c6
ASUS <ASUS@Shubhu>
2022-11-29 12:56:09 +0530

    commit test2 to Branchdemo

---
+++ src/basic/Test2.java
@@ -0,0 +1,22 @@
+package basic;
+
+public class Test2 {
+
+	public static void main(String[] args) 
+	{
+		String hungery="No";
+		if (hungery=="No")
+		{
+			System.out.println("khaay kahitari");
+			
+		} 
+		else
+		{
+			System.out.println("gapp bas");
+
+		}
+		
+
+	}
+
+}
